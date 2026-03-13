d6233f2d7a096917848cfff159bf0dc532f677aa
nebula <nebulageek@protonmail.com>
2026-03-13 20:26:55 +0500

    Create 4A(watermelon).py

---
+++ CodeForces/python/4A(watermelon).py
@@ -0,0 +1,2 @@
+num = int(input()) # get num
+print("YES" if num % 2 == 0 and 2 < num <= 100 else 'NO') # prints YES if num is higher than 2 and the number is even, and if the number is odd prints NO.
