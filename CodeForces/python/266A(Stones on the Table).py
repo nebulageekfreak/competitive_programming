caf1197da77ad867168b41c09d8d3b77fbb08180
nebula <nebulageek@protonmail.com>
2026-03-16 22:12:54 +0500

    Update and rename 266A.py to 266A(Stones on the Table).py
    added another solution which uses enumerate, and also added full name of problem.

--- CodeForces/python/266A(Stones on the Table).py
+++ CodeForces/python/266A(Stones on the Table).py
@@ -5,3 +5,10 @@ for x in range(1, n):
     if stones[x] == stones[x - 1]:
         counter += 1
 print(counter)
+
+# enumerate method
+
+# for i, v in enumerate(stones[1:]):
+#     prev = stones[i]
+#     if v == prev:
+#         counter += 1
