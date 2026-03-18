eddb13469718d90f77ca3e73ebf628f9d306d654
nebula <nebulageek@protonmail.com>
2026-03-18 21:31:50 +0500

    Update 110A(Nearly Lucky Number).py

--- CodeForces/python/110A(Nearly Lucky Number).py
+++ CodeForces/python/110A(Nearly Lucky Number).py
@@ -2,3 +2,22 @@ n = input()
 lucky_nums = sum(1 for x in n if x in '47')
 
 print('YES' if all(x in '47' for x in str(lucky_nums)) else 'NO')
+
+
+# not one line solution:
+
+# n = input()
+
+# lucky_nums = 0
+# result = 'YES'
+
+# for x in n:
+#     if x in '47':
+#         lucky_nums += 1
+
+# for num in str(lucky_nums):
+#     if num not in '47':
+#         result = 'NO'
+#         break
+
+# print(result)
