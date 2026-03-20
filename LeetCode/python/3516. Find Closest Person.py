5bbd45ba31c272b155094cc55c7a7e70a5076fdf
nebula <nebulageek@protonmail.com>
2026-03-20 21:03:33 +0500

    Create 3516. Find Closest Person.py

---
+++ LeetCode/python/3516. Find Closest Person.py
@@ -0,0 +1,9 @@
+# link to this problem: https://leetcode.com/problems/find-closest-person/description/
+
+class Solution:
+    def findClosest(self, x: int, y: int, z: int) -> int:
+        if abs(z - x) < abs(z - y):
+            return 1
+        elif abs(z - x) > abs(z - y):
+            return 2
+        return 0
