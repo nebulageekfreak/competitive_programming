4502b1e93b396047988186b3c3021a2ff47e8eb3
nebulageekfreak <nebulageek@protonmail.com>
2026-03-30 21:56:26 +0500

    solved weird algorithm in CSES Introductury Problems

---
+++ cses/cpp/Introductory Problems/0001 - Weird Algorithm.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    long long n; cin >> n;
+    
+    while (true) {
+        cout << n << ' ';
+        if (n == 1) {
+            
+            break;
+        }
+        
+        if (n % 2  == 0) {
+            n /= 2;
+        }
+        else if (n % 2 == 1) {
+            n = n * 3 + 1;
+        }
+    }
+    return 0;
+}
