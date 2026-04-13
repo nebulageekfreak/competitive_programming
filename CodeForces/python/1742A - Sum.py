0898c7f016af2ab54d91e0bfba872b67d41373e4
nebulageekfreak <nebulageek@protonmail.com>
2026-04-13 09:47:39 +0500

    solved 1742A.py

---
+++ CodeForces/python/1742A - Sum.py
@@ -0,0 +1,9 @@
+n = int(input())
+
+for i in range(n):
+    a, b, c = map(int, input().split())
+    
+    if a + b == c or a + c == b or b + c == a:
+        print("YES")
+    else:
+        print("NO")
